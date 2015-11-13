23b87eae5551549732514741427cf515609a3285
kirilov <bogong@mail.ru>
2015-11-13 14:44:28 +0300

    Adding: json headers

--- Yaws_addons/a_header.erl
+++ Yaws_addons/a_header.erl
@@ -14,6 +14,7 @@
 -export([last_modified/1]).
 -export([expires/1]).
 -export([cache/1]).
+-export([json/1]).
 
 %% System include
 
@@ -22,12 +23,12 @@
 %% Module Include End
 
 %%-----------------------------------
-%% @spec last_modified(Time_in) -> list()
+%% @spec last_modified(Time_in) -> list() | {error,_Reason}.
 %% where
 %%      Time_in() :: integer() | tuple() | current
 %% @doc Return a list() within HTTP Header formated for Yaws out() function.
 %% Example: "Last-Modified: Fri, 30 Oct 1998 14:19:41 GMT"
--spec last_modified(Time_in) -> list()
+-spec last_modified(Time_in) -> list() | {error,_Reason}
 	when Time_in :: pos_integer() | tuple() | current.
 
 last_modified(Time_in) when is_integer(Time_in), Time_in > 0 ->
@@ -47,12 +48,12 @@ last_modified(current) -> last_modified(erlang:localtime());
 last_modified(_) -> a:error(?FUNCTION_NAME(),a000).
 
 %%-----------------------------------
-%% @spec expires(Time_in) -> list().
+%% @spec expires(Time_in) -> list() | {error,_Reason}.
 %% where
 %%      Time_in :: integer() | tuple() | current
 %% @doc Return a list() within HTTP Header formated for Yaws out() function.
 %% Example: "Expires: Fri, 30 Oct 1998 14:19:41 GMT"
--spec expires(Time_in) -> list()
+-spec expires(Time_in) -> list() | {error,_Reason}
 	when Time_in :: pos_integer() | tuple() | current.
 
 expires(Time_in) when is_integer(Time_in) == true, Time_in > 0 ->
@@ -72,11 +73,11 @@ expires(current) -> expires(erlang:localtime());
 expires(_) -> a:error(?FUNCTION_NAME(),a000).
 
 %%-----------------------------------
-%% @spec cache(Operation) -> list()
+%% @spec cache(Operation) -> list() | {error,_Reason}.
 %% where
 %%      Operation() = no
 %% @doc Return a list() within HTTP headers fromated for Yaws out() function.
--spec cache(Operation) -> list()
+-spec cache(Operation) -> list() | {error,_Reason}
 	when Operation :: no.
 
 cache(no) ->
@@ -87,3 +88,19 @@ cache(no) ->
 		last_modified(current)
 	];
 cache(_) -> a:error(?FUNCTION_NAME(),a000).
+
+%%-----------------------------------
+%% @spec json(Header_type) -> list() | {error,_Reason}.
+%% where
+%%      Header_type() = np_cache | solid
+%% @doc Return a list within headers for JSON
+-spec json(no_cache) -> list() | {error,_Reason}.
+
+json(no_cache) ->
+	[
+		cache(no),
+		json(solid)
+	];
+json(solid) ->
+	[{header,["Content-Type","application/json; charset=UTF-8"]}];
+json(_) -> a:error(?FUNCTION_NAME(),a000).
