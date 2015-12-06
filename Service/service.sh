1ddcc41dfe327be2678c6c040d0bc17b79d6f4a7
kirilov <bogong@mail.ru>
2015-12-06 14:39:56 +0300

    Adding: service scripts

---
+++ Service/service.sh
@@ -0,0 +1,166 @@
+#!/usr/bin/env bash
+
+# --------------------------------------------
+# @doc Arboreus Library service script
+# Only for BASH usage, no DASH compatibility
+# @author Alexandr KIRILOV (http://alexandr.kirilov.me)
+# @copyright (C) 2015, Arboreus Library, http://arboreus.system
+# --------------------------------------------
+
+# --------------------------------------------
+# Check required variables and parameters
+# --------------------------------------------
+function require(){
+	if [ -z ${USER_NAME+x} ]; then variable_error "USER_NAME"; fi
+	if [ -z ${NODE_TEST_NAME+x} ]; then variable_error "NODE_API_TEST_NAME"; fi
+	if [ -z ${NODE_NAME+x} ]; then variable_error "NODE_API_NAME"; fi
+	if [ -z ${DIR_ROOT+x} ]; then variable_error "DIR_ROOT"; fi
+	if [ -z ${DIR_SRC+x} ]; then variable_error "DIR_SRC"; fi
+	if [ -z ${DIR_BIN+x} ]; then variable_error "DIR_BIN"; fi
+	if [ -z ${DIR_OLD_BIN+x} ]; then variable_error "DIR_OLD_BIN"; fi
+	if [ ! -d "$DIR_ROOT" ]; then
+		printf "Error: no defined root direcory, check configuration file";
+		exit 1
+	fi
+}
+
+# --------------------------------------------
+# Variables checking error handler
+# --------------------------------------------
+function variable_error(){
+	if [ $1 ]; then
+		printf "Error: variable $1 does not set, check configuration file\n";
+		exit 1;
+	else
+		printf "Error: execution error_var(), no id_var\n";
+		exit 1;
+	fi
+}
+
+# --------------------------------------------
+# Checking exclusions
+# --------------------------------------------
+function exclusion(){
+	CHECK="";
+	if [ $1 == "--dir" ];
+	then
+		if [ ! -z ${EXCLUSION_DIRS+x} ];
+		then
+			for DIR in ${EXCLUSION_DIRS}
+			do
+				if [ ${DIR} == $2 ];
+				then CHECK="true"; break;
+				else CHECK="false"; fi
+			done
+		else
+			CHECK="false";
+		fi
+	elif [ $1 == "--file" ];
+	then
+		if [ ! -z ${EXCLUSION_FILES+x} ];
+		then
+			for FILE in ${EXLUSION_FILES}
+			do
+				if [ ${FILE} == $2 ];
+				then CHECK="true"; break;
+				else CHECK="false"; fi
+			done
+		else
+			CHECK="false";
+		fi
+	fi
+	if [ ${CHECK} == "false" ];
+	then echo "false"; else echo "true"; fi
+}
+
+# --------------------------------------------
+# Project compilation
+# --------------------------------------------
+function compile(){
+	if [ -d "$DIR_BIN" ];
+	then
+		if [ -d "$DIR_OLD_BIN" ];
+		then
+			rm -rf ${DIR_OLD_BIN};
+		fi
+		mv ${DIR_BIN} ${DIR_OLD_BIN};
+		mkdir -p ${DIR_BIN};
+	else
+		mkdir -p ${DIR_BIN};
+	fi
+	cd ${DIR_SRC};
+	FILES_ERL=$(find . -name '*.erl');
+	for FILE_PATH_SOURCE in ${FILES_ERL}
+	do
+		FILE_PATH=${FILE_PATH_SOURCE:1};
+		IFS="/" read -ra PATH_DEVIDED <<< "$FILE_PATH";
+		FILE_NAME_POSITION=$((${#PATH_DEVIDED[@]}-1));
+		FILE_NAME=${PATH_DEVIDED[$FILE_NAME_POSITION]};
+		MODULE_NAME=$(echo $FILE_NAME | sed "s/.erl$//");
+		RELATIVE_PATH=$(echo $FILE_PATH | sed "s/\/$FILE_NAME$//");
+		EXCLUSION_DIR=$(exclusion --dir ${RELATIVE_PATH});
+		EXCLUSION_FILE=$(exclusion --file ${RELATIVE_PATH}${FILE_NAME});
+		if [ ${EXCLUSION_DIR} == "false" ]; then
+			if [ ${EXCLUSION_FILE} == "false" ]; then
+				if [ ! -d "$DIR_BIN$RELATIVE_PATH" ]; then
+					mkdir -p ${DIR_BIN}${RELATIVE_PATH};
+				fi
+				erlc -o ${DIR_BIN}${RELATIVE_PATH} ${DIR_SRC}${FILE_PATH};
+				printf "Done! $FILE_PATH\n"
+			fi
+		fi
+	done
+	printf "***\nDone! Erlang source compiled\n";
+}
+
+function update(){
+	echo "update"
+}
+
+function help(){
+	echo "help"
+}
+
+# --------------------------------------------
+# Script actions
+# --------------------------------------------
+if [ $1 ];
+then
+	if [ $1 == "--conf" ];
+	then
+		if [ ! -f $2 ];
+		then
+			printf "Error: no defined configuration file: $2\n";
+		else
+			source $2;
+			require;
+			if [ $(whoami) != ${USER_NAME} ]; then
+				printf "Error: current user have no privileges to run this script\n";
+				exit 1;
+			fi
+			if [ ! $3 ]; then
+				printf "Error: no action parameter\n";
+				exit 1;
+			fi
+			if [ $3 == "--do" ]
+			then
+				if [ ! $4 ]; then
+					printf "Error: action must be defined\n";
+					exit 1;
+				fi
+				if [ $4 == "compile" ]; then compile;
+				elif [ $4 == "update" ]; then update;
+				else
+					printf "Error: wrong action defined\n";
+				fi
+			else
+				printf "Error: action parameter wrong\n";
+			fi
+		fi
+	elif [ $1 == "--help" ]; then help;
+	else
+		printf "Error: you should define configuration file at first\n";
+	fi
+else
+	printf "Error: no any parameters found\n";
+fi
