f97e1526d9ae7acef62b27ee729285abbba2a88b
kirilov <bogong@mail.ru>
2016-02-06 12:59:58 +0300

    Fixing bug: a_net:ipv4_to_integer

--- src/Handler/error_codes.hrl
+++ src/Handler/error_codes.hrl
@@ -20,7 +20,7 @@
 	{a015,"Bad arguments, must be typified list()"},
 	{a016,"Throw exeption"},
 	{a017,"Wrong path"},
-	{a004,"Bad arguments, must be string()"},
+	{a018,"Bad arguments, must be typified string()"},
 
 	%% Error handler
 	{e000,"Error handler: bad arguments"},
--- src/Universal/a_net.erl
+++ src/Universal/a_net.erl
@@ -8,7 +8,7 @@
 %%%-------------------------------------------------------------------
 -module(a_net).
 -author("Alexandr KIRILOV, http://alexandr.kirilov.me").
--vsn("0.0.1.185").
+-vsn("0.0.2.193").
 
 %% API
 -export([
@@ -19,22 +19,28 @@
 -include("../Handler/a.hrl").
 %% Module Include End
 
+
 %%-----------------------------------
-%% @doc Return integer from IP
+%% @doc Return integer from IPv4
 -spec ipv4_to_integer(Ip) -> integer() | {error,_Reason}
 	when
-		Ip :: tuple() | list().
+		Ip :: tuple() | list() | string().
 
-ipv4_to_integer({A,B,C,D})
-	when
-		is_integer(A),is_integer(B),is_integer(C),is_integer(D),
-		A >= 0, A =< 255, B >= 0, B =< 255,
-		C >= 0, C =< 255, D >= 0, D =< 255 ->
-	(A*16777216)+(B*65536)+(C*256)+(D);
+ipv4_to_integer({A,B,C,D}) -> ipv4_to_integer([A,B,C,D]);
 ipv4_to_integer([A,B,C,D])
 	when
-		is_integer(A),is_integer(B),is_integer(C),is_integer(D),
+		is_integer(A),is_integer(B),
+		is_integer(C),is_integer(D),
 		A >= 0, A =< 255, B >= 0, B =< 255,
 		C >= 0, C =< 255, D >= 0, D =< 255 ->
 	(A*16777216)+(B*65536)+(C*256)+(D);
+ipv4_to_integer(Ip_string) when is_list(Ip_string) ->
+	case io_lib:char_list(Ip_string) of
+		true ->
+			try
+				{ok,Ip_tuple} = inet:parse_ipv4_address(Ip_string),
+				ipv4_to_integer(Ip_tuple)
+			catch _:_ -> a:error(?FUNCTION_NAME(),a018) end;
+		_ -> a:error(?FUNCTION_NAME(),a003)
+	end;
 ipv4_to_integer(_) -> a:error(?FUNCTION_NAME(),a003).
