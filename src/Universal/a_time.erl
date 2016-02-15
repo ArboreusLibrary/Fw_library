8cf88786bd8f797d2de3838be10642241937ccdb
kirilov <bogong@mail.ru>
2016-02-15 15:46:35 +0300

    Adding: convert time from fromated ansi, rfc850, rfc822

--- src/Universal/a_time.erl
+++ src/Universal/a_time.erl
@@ -8,18 +8,22 @@
 %%%-------------------------------------------------------------------
 -module(a_time).
 -author("Alexandr KIRILOV (http://alexandr.kirilov.me)").
--vsn("0.0.13.224").
+-vsn("0.0.14.226").
 
 %% Module API
 -export([
 	current_date/0,current_year/1,current_month/0,current_day/0,current_dow/1,
 	current/0,current/1,
 	timestamp/0,timestamp/1,timestamp_to_tuple/1,
+	second/1,
+	minute/1,
+	hour/1,
 	day/1,
 	dow/1,dow/2,
 	month/1,month/2,
 	year/2,
-	format/2
+	format/2,
+	from_formated/3
 ]).
 
 %% Module Include Start
@@ -37,6 +41,7 @@
 
 current_date() -> {Date,_}=erlang:localtime(), Date.
 
+
 %%-----------------------------------
 %% @doc Return a Year :: integer(), the part of erlang:localtime()
 -spec current_year(Output_type) -> integer() | {error,_Error_notice} when
@@ -46,17 +51,20 @@ current_year(full) -> {Year,_,_} = current_date(), Year;
 current_year(short) -> current_year(full) - trunc(current_year(full)/100)*100;
 current_year(_) -> a:error(?FUNCTION_NAME(),a011).
 
+
 %%-----------------------------------
 %% @doc Return a Month() = integer(), the part of erlang:localtime()
 -spec current_month() -> integer().
 
 current_month() -> {_,Month,_} = current_date(), Month.
 
+
 %%-----------------------------------
 %% @doc Return a Day :: integer(), the part of erlang:localtime()
 -spec current_day() -> integer().
 
 current_day() -> {_,_,Day} = current_date(), Day.
+
 
 %%-----------------------------------
 %% @doc Return the current day of the week within binaries
@@ -71,11 +79,13 @@ current_dow(View)
 	dow(calendar:day_of_the_week(current_date()),View);
 current_dow(_) -> a:error(?FUNCTION_NAME(),a012).
 
+
 %%-----------------------------------
 %% @doc Return a Time() within a tuple() = {Hours,Minutes,Seconds}, the part of erlang:localtime()
 -spec current() -> tuple().
 
 current() -> {_,Time}=erlang:localtime(), Time.
+
 
 %%-----------------------------------
 %% @doc Return a binary within the current time formated by the Format()::atom() from the list
@@ -100,6 +110,7 @@ current(_) -> a:error(?FUNCTION_NAME(),a012).
 timestamp(binaries) -> integer_to_binary(timestamp());
 timestamp(_) -> a:error(?FUNCTION_NAME(),a012).
 
+
 %%-----------------------------------
 %% @doc Return current timestamp as integer
 -spec timestamp() -> integer().
@@ -107,6 +118,7 @@ timestamp(_) -> a:error(?FUNCTION_NAME(),a012).
 timestamp() ->
 	{Mega,Sec,Micro}=os:timestamp(),
 	Mega*1000000000000+Sec*1000000+Micro.
+
 
 %%-----------------------------------
 %% @doc Return a tuple within converted Timestamp from integer
@@ -119,9 +131,106 @@ timestamp_to_tuple(Timestamp) when is_integer(Timestamp) == true, Timestamp > 0 
 	{Mega,Sec,Micro};
 timestamp_to_tuple(_) -> a:error(?FUNCTION_NAME(),a009).
 
+
+%%-----------------------------------
+%% @doc return integer within timestamp from fromated date tuple
+-spec to_timestamp(Data) -> integer() | {error,_Reason}
+	when
+		Data :: {{Year,Month,Day},{Hours,Minutes,Seconds}},
+		Year :: integer(), Month :: integer(), Day :: integer(),
+		Hours :: integer(), Minutes :: integer(), Seconds :: integer().
+
+to_timestamp({{Year,Month,Day},{Hours,Minutes,Seconds}}) ->
+	(calendar:datetime_to_gregorian_seconds(
+		{{Year,Month,Day},{Hours,Minutes,Seconds}}
+	) - 62167219200)*1000000;
+to_timestamp(_) -> a:error(?FUNCTION_NAME(),a000).
+
+
 %% ------------------------------------------------
-%% Day of the week
+%% Time
 %% ------------------------------------------------
+
+%%-----------------------------------
+%% @doc
+-spec second(Second) -> integer() | {error,_Reason}
+	when
+		Second :: string() | unicode:latin1_binary().
+
+second(Second) when Second == <<"0">>, Second == <<"00">>, Second == "0", Second == "00" -> 0;
+second(Second) when Second == <<"1">>, Second == <<"01">>, Second == "1", Second == "01" -> 1;
+second(Second) when Second == <<"2">>, Second == <<"02">>, Second == "2", Second == "02" -> 2;
+second(Second) when Second == <<"3">>, Second == <<"03">>, Second == "3", Second == "03" -> 3;
+second(Second) when Second == <<"4">>, Second == <<"04">>, Second == "4", Second == "04" -> 4;
+second(Second) when Second == <<"5">>, Second == <<"05">>, Second == "5", Second == "05" -> 5;
+second(Second) when Second == <<"6">>, Second == <<"06">>, Second == "6", Second == "06" -> 6;
+second(Second) when Second == <<"7">>, Second == <<"07">>, Second == "7", Second == "07" -> 7;
+second(Second) when Second == <<"8">>, Second == <<"08">>, Second == "8", Second == "08" -> 8;
+second(Second) when Second == <<"9">>, Second == <<"09">>, Second == "9", Second == "09" -> 9;
+second(Second) when is_binary(Second) ->
+	try
+		Second_integer = binary_to_integer(Second),
+		if
+			Second_integer >= 0, Second_integer =< 59 -> Second_integer;
+			true -> a:error(?FUNCTION_NAME(),a000)
+		end
+	catch _:_ -> a:error(?FUNCTION_NAME(),a000) end;
+second(Second) when is_list(Second) ->
+	case io_lib:char_list(Second) of
+		true ->
+			Second_integer = list_to_integer(Second),
+			if
+				Second_integer >= 0, Second_integer =< 59 -> Second_integer;
+				true -> a:error(?FUNCTION_NAME(),a000)
+			end
+	end;
+second(_) -> a:error(?FUNCTION_NAME(),a000).
+
+
+%%-----------------------------------
+%% @doc Return integer from formated minute
+-spec minute(Minute) -> integer() | {error,_Reason}
+	when
+		Minute :: string() | unicode:latin1_binary().
+
+minute(Minute) -> second(Minute).
+
+
+%%-----------------------------------
+%% @doc Return integer from formated hour
+-spec hour(Hour) -> integer() | {error,_Reason}
+	when
+		Hour :: string() | unicode:latin1_binary().
+
+hour(Hour) when Hour == <<"0">>, Hour == <<"00">>, Hour == "0", Hour == "00" -> 0;
+hour(Hour) when Hour == <<"1">>, Hour == <<"01">>, Hour == "1", Hour == "01" -> 1;
+hour(Hour) when Hour == <<"2">>, Hour == <<"02">>, Hour == "2", Hour == "02" -> 2;
+hour(Hour) when Hour == <<"3">>, Hour == <<"03">>, Hour == "3", Hour == "03" -> 3;
+hour(Hour) when Hour == <<"4">>, Hour == <<"04">>, Hour == "4", Hour == "04" -> 4;
+hour(Hour) when Hour == <<"5">>, Hour == <<"05">>, Hour == "5", Hour == "05" -> 5;
+hour(Hour) when Hour == <<"6">>, Hour == <<"06">>, Hour == "6", Hour == "06" -> 6;
+hour(Hour) when Hour == <<"7">>, Hour == <<"07">>, Hour == "7", Hour == "07" -> 7;
+hour(Hour) when Hour == <<"8">>, Hour == <<"08">>, Hour == "8", Hour == "08" -> 8;
+hour(Hour) when Hour == <<"9">>, Hour == <<"09">>, Hour == "9", Hour == "09" -> 9;
+hour(Hour) when is_binary(Hour) ->
+	try
+		Hour_integer = binary_to_integer(Hour),
+		if
+			Hour_integer >= 0, Hour_integer =< 23 -> Hour_integer;
+			true -> a:error(?FUNCTION_NAME(),a000)
+		end
+	catch _:_ -> a:error(?FUNCTION_NAME(),a000) end;
+hour(Hour) when is_list(Hour) ->
+	case io_lib:char_list(Hour) of
+		true ->
+			Hour_integer = list_to_integer(Hour),
+			if
+				Hour_integer >= 1, Hour_integer =< 31 -> Hour_integer;
+				true -> a:error(?FUNCTION_NAME(),a000)
+			end
+	end;
+hour(_) -> a:error(?FUNCTION_NAME(),a000).
+
 
 %%-----------------------------------
 %% @doc Return integer within converted day
@@ -517,3 +626,50 @@ format(sec,Seconds) when is_integer(Seconds) == true, Seconds > 9, Seconds =< 59
 format(sec,_) -> a:error(?FUNCTION_NAME(),a009);
 
 format(_,_) -> a:error(?FUNCTION_NAME(),a000).
+
+
+%%-----------------------------------
+%% @doc Return timestamp/data()/seconds from formated time
+-spec from_formated(Format_type,Time_source,Output_type) -> tuple() | integer() | false | {error,_Reason}
+	when
+		Format_type :: ansi | rfc850 | rfc822 | date_tuple,
+		Time_source :: string() | unicode:latin1_binary() | tuple(),
+		Output_type :: tuple | seconds | timestamp.
+
+from_formated(Format_type,Time_source,Output_type) when is_list(Time_source) ->
+	case io_lib:char_list(Time_source) of
+		true -> from_formated(Format_type,unicode:characters_to_binary(Time_source),Output_type);
+		_ -> a:error(?FUNCTION_NAME(),a000)
+	end;
+from_formated(ansi,Time_source,Output_type) when is_binary(Time_source) ->
+	Pattern = <<"^([A-Za-z]{3}) ([A-Za-z]{3}) ([0-9]{2}) ([0-9]{2})\:([0-9]{2})\:([0-9]{2}) ([0-9]{4})$">>,
+	case re:split(Time_source,Pattern,[{return,list}]) of
+		[_,_,Month,Day,Hours,Minutes,Seconds,Year,_] ->
+			from_formated(date_tuple,{{year(to_integer,Year),month(Month),day(Day)},
+				{hour(Hours),minute(Minutes),second(Seconds)}},Output_type);
+		_ -> false
+	end;
+from_formated(rfc850,Time_source,Output_type) when is_binary(Time_source) ->
+	Pattern = <<"^[a-zA-Z]{0,10}\, ([0-9]{2})\-([a-zA-Z]{3})\-([0-9]{2}) ([0-9]{2})\:([0-9]{2})\:([0-9]{2}) GMT$">>,
+	case re:split(Time_source,Pattern,[{return,list}]) of
+		[[],Day,Month,Year,Hours,Minutes,Seconds,[]] ->
+			from_formated(date_tuple,{{year(to_integer,Year)+2000,month(Month),day(Day)},
+				{hour(Hours),minute(Minutes),second(Seconds)}},Output_type);
+		_ -> false
+	end;
+from_formated(rfc822,Time_source,Output_type) when is_binary(Time_source) ->
+	Pattern = <<"^[a-zA-Z]{3}\, ([0-9]{2}) ([a-zA-Z]{3}) ([0-9]{4}) ([0-9]{2})\:([0-9]{2})\:([0-9]{2}) GMT$">>,
+	case re:split(Time_source,Pattern,[{return,list}]) of
+		[_,Day,Month,Year,Hours,Minutes,Seconds,_] ->
+			from_formated(date_tuple,{{year(to_integer,Year),month(Month),day(Day)},
+				{hour(Hours),minute(Minutes),second(Seconds)}},Output_type);
+		_ -> false
+	end;
+from_formated(date_tuple,{{Year,Month,Day},{Hours,Minutes,Seconds}},Output_type) ->
+	case Output_type of
+		tuple -> {{Year,Month,Day},{Hours,Minutes,Seconds}};
+		seconds -> calendar:datetime_to_gregorian_seconds({{Year,Month,Day},{Hours,Minutes,Seconds}});
+		timestamp -> to_timestamp({{Year,Month,Day},{Hours,Minutes,Seconds}});
+		_ -> a:error(?FUNCTION_NAME(),a012)
+	end;
+from_formated(_,_,_) -> a:error(?FUNCTION_NAME(),a000).
