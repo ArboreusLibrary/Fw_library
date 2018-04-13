51f0bea859dc176a50a31adfbd87f120d3f5593a
kirilov <bogong@mail.ru>
2018-04-13 13:06:36 +0300

    Develop a_code:l

--- src/universal/a_code.erl
+++ src/universal/a_code.erl
@@ -14,10 +14,46 @@
 
 %% API
 -export([
+	test/0,
 	is_module/1,
-	is_path/1
+	is_path/1,
+	l/1
 ]).
 
+
+%% ----------------------------
+%% @doc Module test function
+-spec test() -> ok.
+
+test() -> ok.
+
+
+%% ----------------------------
+%% @doc Wrapper for l_handler/2
+-spec l(Modules) -> {ok,Output}
+	when
+	Modules :: [Module],
+	Output :: [{Module,code:load_ret()}],
+	Module :: atom().
+
+l(Modules) -> l_handler(Modules,[]).
+
+
+%% ----------------------------
+%% @doc Reload modules from list of module's names
+-spec l_handler(Modules,Output) -> {ok,Output}
+	when
+	Modules :: [Module],
+	Output :: [{Module,code:load_ret()}],
+	Module :: atom().
+
+l_handler([],Output) -> {ok,Output};
+l_handler([Module|Modules],Output) ->
+	l_handler(
+		Modules,
+		lists:append(Output,[{Module,c:l(Module)}])
+	).
+	
 
 %% ----------------------------
 %% @doc Check the module for presence in the Erlang environment.
