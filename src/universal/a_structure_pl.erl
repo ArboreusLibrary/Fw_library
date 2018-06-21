011b56ef7ca854348131b1348dac13f8b5ba977d
kirilov <bogong@mail.ru>
2018-06-21 21:42:17 +0300

    Initiate data structures handler based on records, tuple, proplist, map

---
+++ src/universal/a_structure_pl.erl
@@ -0,0 +1,42 @@
+%%%-------------------------------------------------------------------
+%%% @author Alexandr KIRILOV
+%%% @copyright (C) 2018, http://arboreus.system
+%%% @doc Arboreus prop-list based data structures handler
+%%%
+%%% @end
+%%% Created : 06/21/2018 at 21:39
+%%%-------------------------------------------------------------------
+-module(a_structure_pl).
+-author("Alexandr KIRILOV, http://alexandr.kirilov.me").
+
+%% Constants
+
+%% Data types
+
+%% Data models
+
+%% API
+-export([
+	test/0
+]).
+
+
+%% ----------------------------
+%% @doc Module test function
+-spec test() -> ok.
+
+test() ->
+	Time_start = a_time:current(timestamp),
+	io:format("*** -------------------~n"),
+	io:format(
+		"Module (a_structure_pl) testing started at:~n~p (~p)~n",
+		[a_time:from_timestamp(rfc850, Time_start), Time_start]
+	),
+	Time_stop = a_time:current(timestamp),
+	io:format("*** -------------------~n"),
+	io:format(
+		"Module (a_structure_pl) testing finished at:~n~p (~p)~n",
+		[a_time:from_timestamp(rfc850, Time_stop), Time_stop]
+	),
+	io:format("Test time is: ~p~n", [Time_stop - Time_start]),
+	ok.
