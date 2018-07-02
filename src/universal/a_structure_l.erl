6d8a63156b46cdeac2a5e5672e7849bd59fd7d26
kirilov <bogong@mail.ru>
2018-07-02 13:21:54 +0300

    Develop a_structure_l:reference/3

--- src/universal/a_structure_l.erl
+++ src/universal/a_structure_l.erl
@@ -21,7 +21,9 @@
 	test/0,
 	verify/3,
 	mass_verify/2,mass_verify/3,
-	model/2
+	model/2,
+	reference/1,reference/2,reference/3,
+	elements/2
 ]).
 
 
@@ -36,8 +38,9 @@ test() ->
 		"Module (a_structure_l) testing started at:~n~p (~p)~n",
 		[a_time:from_timestamp(rfc850, Time_start), Time_start]
 	),
-	List1 = [1,atom,0.1,"123"],
-	List2 = [2,second_atom,0.2,"1234"],
+	List1 = [1,atom,0.1,"11"],
+	List2 = [2,second_atom,0.1,"11"],
+	List3 = [3,third_atom,0.1,"22"],
 	List_wrong = [one,atom,0.1,"123"],
 	Model1 = [(fun is_integer/1),(fun is_atom/1),(fun is_float/1),(fun is_list/1)],
 	true = verify(return_boolean,Model1,List1),
@@ -50,19 +53,24 @@ test() ->
 	false = verify(return_boolean,Model2,List_wrong),
 	true = verify(return_boolean,Model2,List2),
 	io:format("DONE! Fun model/2 test passed~n"),
-	List_of_structures = [List1,List2,List1],
-	List_of_structures_wrong = [List1,List2,List_wrong],
-	true = mass_verify(Model1,List_of_structures),
-	true = mass_verify(Model2,List_of_structures),
-	false = mass_verify(Model1,List_of_structures_wrong),
-	false = mass_verify(Model2,List_of_structures_wrong),
+	Structures = [List1,List2,List3],
+	Structures_wrong = [List1,List2,List_wrong],
+	true = mass_verify(Model1,Structures),
+	true = mass_verify(Model2,Structures),
+	false = mass_verify(Model1,Structures_wrong),
+	false = mass_verify(Model2,Structures_wrong),
 	false = mass_verify(Model1,[]),
 	true = mass_verify([],[]),
-	false = mass_verify([],List_of_structures),
+	false = mass_verify([],Structures),
 	io:format("DONE! Fun mass_verify/2 test passed~n"),
-	{true,List_of_structures} = mass_verify(return_list,Model1,List_of_structures),
-	{true,List_of_structures} = mass_verify(return_list,Model2,List_of_structures),
+	{true,Structures} = mass_verify(return_list,Model1,Structures),
+	{true,Structures} = mass_verify(return_list,Model2,Structures),
 	io:format("DONE! Fun mass_verify/3 test passed~n"),
+	{true,Reference1} = reference(Structures),
+	{true,Reference1} = reference(Structures,all),
+	{true,Reference1} = reference(Structures,all,[]),
+	{true,Reference1} = reference(Structures,[1,2,3,4],[]),
+	io:format("DONE! Fun reference/3 test passed: ~p~n",[Reference1]),
 	Time_stop = a_time:current(timestamp),
 	io:format("*** -------------------~n"),
 	io:format(
@@ -71,6 +79,118 @@ test() ->
 	),
 	io:format("Test time is: ~p~n", [Time_stop - Time_start]),
 	ok.
+
+
+%% ----------------------------
+%% @doc Wrapper for reference/2
+-spec reference(Structures) -> false | {true,Reference}
+	when
+	Structures :: list(),
+	Reference :: proplists:proplist().
+
+reference(Structures) -> reference(Structures,all).
+
+
+%% ----------------------------
+%% @doc Wrapper for reference/3
+-spec reference(Structures,Positions) -> false | {true,Reference}
+	when
+	Structures :: list(),
+	Positions :: list() | all,
+	Reference :: proplists:proplist().
+	
+reference(Structures,Positions) ->
+	reference(Structures,Positions,[]).
+
+%% ----------------------------
+%% @doc Generate reference
+-spec reference(Structures,Positions,Reference) ->
+	false | {true,Reference}
+	when
+	Structures :: list(),
+	Positions :: list() | all,
+	Reference :: proplists:proplist().
+
+reference(Structures,all,Reference) ->
+	[Etalon|_] = Structures,
+	reference_handler(
+		Structures,model(verificator,Etalon),
+		lists:seq(1,length(Etalon)),Reference
+	);
+reference(Structures,Positions,Reference) ->
+	[Etalon|_] = Structures,
+	reference_handler(
+		Structures,model(verificator,Etalon),
+		Positions,Reference
+	).
+
+
+%% ----------------------------
+%% @doc Generating reference procedure handler
+-spec reference_handler(Structures,Model,Positions,Reference) ->
+	false | {true,Reference}
+	when
+	Structures :: list(),
+	Model :: list(),
+	Positions :: list_of_integers(),
+	Reference :: proplists:proplist().
+
+reference_handler([],_,_,Reference) -> {true,Reference};
+reference_handler([Structure|Structures],Model,Positions,Reference) ->
+	case verify(return_boolean,Model,Structure) of
+		true ->
+			Compose_reference = fun
+				Function([],Reference_income) -> Reference_income;
+				Function([{Key,Value}|List],Reference_income) ->
+					case proplists:get_value(Key,Reference_income) of
+						undefined ->
+							Function(List,lists:append(Reference_income,[{Key,[Value]}]));
+						Reference_value ->
+							Function(List,lists:keyreplace(
+								Key,1,Reference_income,{Key,begin
+									case lists:member(Value,Reference_value) of
+										false -> lists:append(Reference_value,[Value]);
+										_ -> Reference_value
+									end
+								end
+							}))
+					end
+			end,
+			reference_handler(
+				Structures,Model,Positions,
+				Compose_reference(elements(Positions,Structure),Reference)
+			);
+		Verification_result -> Verification_result
+	end.
+
+
+%% ----------------------------
+%% @doc Wrapper for elements/3
+-spec elements(Positions,Structure) -> proplists:proplist()
+	when
+	Positions :: list_of_integers(),
+	Structure :: list().
+
+elements(Positions,Structure) ->
+	elements(Positions,Structure,[]).
+
+
+%% ----------------------------
+%% @doc Return proplist within position-value pair of the structure
+-spec elements(Positions,Structure,Elements) -> proplists:proplist()
+	when
+	Positions :: list_of_integers(),
+	Structure :: list(),
+	Elements :: proplists:proplist().
+
+elements([],_,Elements) -> Elements;
+elements([Position|Positions],Structure,Elements) ->
+	elements(
+		Positions,Structure,
+		lists:append(Elements,[
+			{Position,lists:nth(Position,Structure)}
+		])
+	).
 
 
 %% ----------------------------
